5dc9df8ea71508be878ae43eac1fee869c113339
zlatin-r <zlatinrusev@gmail.com>
2025-02-13 14:31:02 +0100

    fix port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ studentsController.setup(app, students);
 
 let port = process.argv[2];
 if (!port) port = process.env['PORT'];
-if (!port) port = 8081;
+if (!port) port = 3030;
 
 app.listen(port, () => {
   console.log(`App started. Listening at http://localhost:${port}`);
